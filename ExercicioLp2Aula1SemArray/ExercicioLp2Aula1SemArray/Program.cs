15c6526bbfc307c132ae7478161a5c36c6beb153
FelipeBorowik <Felipeborowik@gmail.com>
2015-03-25 14:03:53 -0300

    Exercicio da primeira semana de aula, mas sem o array

---
+++ ExercicioLp2Aula1SemArray/ExercicioLp2Aula1SemArray/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioLp2Aula1SemArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbers = 0;
+            int average;
+            int n = 1;
+            int i = 0;
+
+            while (n != 0)
+            {
+                Console.WriteLine("Defina um valor para o N. Para obter o resultado digite 0 ");
+                n = int.Parse(Console.ReadLine());
+
+                numbers += n;
+                i++;
+            }
+
+            Console.WriteLine();
+            average = numbers / (i - 1);
+
+            Console.WriteLine("A média dos numeros digitados é " + average);
+        }
+    }
+}
