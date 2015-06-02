2ce7559490e480afd48fa12cdf7c662f97241247
FelipeBorowik <FelipeBorowik@gmail.com>
2015-06-02 14:59:34 -0300

    A questão da prova, feita em aula

---
+++ A questão de Palindromo da Prova/Stringaocontrario/Stringaocontrario/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+namespace Stringaocontrario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         String Odnaliab = "";
+         String Bailando;
+         Bailando = Console.ReadLine();
+         for (int i = Bailando.Length -1; i >=0 ; i--)
+         {
+             Odnaliab += Bailando[i].ToString();
+         }
+
+         if (Bailando == Odnaliab)
+         {
+             Console.WriteLine("A Palavra digitada é palindromo");
+
+         }
+         else
+
+             Console.WriteLine("A Palavra digitada não é palindromo");
+            
+        }
+    }
+}
---
+++ Exercicios/Stringaocontrario/Stringaocontrario/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+namespace Stringaocontrario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         String Bailando;
+         Bailando = Console.ReadLine();
+         for (int i = Bailando.Length -1; i >=0 ; i--)
+         {
+             Console.WriteLine(Bailando[i]);
+         }
+  
+            
+        }
+    }
+}
