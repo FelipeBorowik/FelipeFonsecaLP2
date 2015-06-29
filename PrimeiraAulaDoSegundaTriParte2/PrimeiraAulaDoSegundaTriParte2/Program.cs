ea97b4991fe2dd882e68d0f87ee325f9420a64a5
FelipeBorowik <Felipeborowik@gmail.com>
2015-06-29 17:11:14 -0300

    Commit da questão da ultima aula, com classes

---
+++ PrimeiraAulaDoSegundaTriParte2/PrimeiraAulaDoSegundaTriParte2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeiraAulaDoSegundaTriParte2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int workersinfo = int.Parse(Console.ReadLine());
+            double newsalary = 0;
+
+            Worker w = new Worker();
+
+
+            for (int i = 0; i < workersinfo; i++)
+            {
+                w.cpf.Add(Console.ReadLine());
+                w.name.Add(Console.ReadLine());               
+                w.borndate.Add(Console.ReadLine());
+                w.entrydate.Add(Console.ReadLine());
+                w.salary.Add(Console.ReadLine());
+                Console.WriteLine();
+            }
+
+            for (int i = 0; i < workersinfo; i++)
+            {
+                Console.WriteLine("CPF: " + w.cpf[i]);
+                Console.WriteLine("Nome: " + w.name[i]);
+                Console.WriteLine("Nasc: " + w.borndate[i]);
+                Console.WriteLine("Admins: " + w.entrydate[i]);
+                Console.WriteLine("Salario Atual: " + w.salary[i]);
+
+                newsalary = w.calculationfornewsalary(i);
+
+                Console.WriteLine("Novo Salário: " + newsalary);
+                Console.WriteLine();
+                
+            }
+        }
+    }
+}
---
+++ PrimeiraAulaDoSegundaTriParte2/PrimeiraAulaDoSegundaTriParte2/Worker.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace PrimeiraAulaDoSegundaTriParte2
+{
+    class Worker
+    {
+        public ArrayList cpf = new ArrayList();
+        public ArrayList borndate = new ArrayList();
+        public ArrayList entrydate = new ArrayList();
+        public ArrayList salary = new ArrayList();
+        public ArrayList name = new ArrayList();
+
+        public double calculationfornewsalary(int j)
+        {
+            double modifiedsalary = 0;
+
+            if (Convert.ToInt32(entrydate[j]) >= 2014)
+            {
+                modifiedsalary = Convert.ToDouble(salary[j]) + 423.45;
+            }
+            else if (Convert.ToInt32(entrydate[j]) >= 2010 && Convert.ToInt32(entrydate[j])<= 2013)
+            {
+                double addiciontosalary = Convert.ToDouble(salary[j]) * 9.84 / 100;
+                modifiedsalary = Convert.ToDouble(salary[j]) + addiciontosalary;
+            }
+            else if (Convert.ToInt32(entrydate[j]) >= 2000 && Convert.ToInt32(entrydate[j]) <= 2010)
+            {
+                double addiciontosalary1 = Convert.ToDouble(salary[j])* 16.4 / 100;
+                modifiedsalary = Convert.ToDouble(salary[j]) + addiciontosalary1;
+            }
+            else if (Convert.ToInt32(entrydate[j]) < 2000)
+            {
+                 double addiciontosalary2 = Convert.ToDouble(salary[j])* 22.6 / 100;
+                 modifiedsalary = Convert.ToDouble(salary[j]) + addiciontosalary2;
+            }
+
+            return modifiedsalary;
+        }
+    }
+}
